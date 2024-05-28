f812d9005a6e9a6fd1af621df5d970f015d2a760
luka081009 <lukarijhamadze@gmail.com>
2024-05-28 19:17:15 +0400

    commit

---
+++ level 3/classwork/level3.py
@@ -0,0 +1,63 @@
+# print('hallo chads')
+# print(100)
+
+
+# text/ტექსტი = streeng/სტრინგი
+# number/რიცხვი = intejer/ინტეჯერი
+
+
+
+# ვცვლადის - დანიშნულება არის ის, რომ მან შეინახოს ნებისმიერი ტიპის მნიშვნელობა.
+# ჩბენ შეგვიძლია, რომ ცვლადები გამოვიყენოთ სხვადასხვა კოდში.
+
+# name = 'GOA'
+
+# proffesion = 'Web Developer + Mentor'
+
+# last_name = 'goadze'
+
+# age = 30
+
+# friend_name = 'anastasia'
+
+# bounty = '1 server mute'
+
+# print(name, proffesion, last_name, age, friend_name, bounty)
+
+# გვარი = 'ბასილაძე'
+
+# print(გვარი)
+
+# data types
+# 1) String
+# 2)nInteger
+
+# x = 10
+# y = 20
+
+# x = 550
+# y = 150
+
+#print(x + y)
+
+# name = 'dachi'
+
+# name = 'nikolozi'
+# name = 'gela'
+# name = 'angelina'
+
+# print(name)
+
+# print(10 + 10) # მიმატება
+# print(20 - 10) # გამოკლება
+# print(5 * 5) # გამრავლება
+# print(10 / 2) # გაყოფა
+# print(10 // 2)
+# print(13 / 2)
+# print(13 // 2)
+# print(5 ** 5) # 5 * 5 * 5 * 5 * 5
+# / - გამოიყენება იმისათვის, რომ პასუხი მივიღოთ ათწილადი
+# // - გამოიყენება იმისათვის, რომ პასუხი მივიღოთ მთელი რიცხვი
+ 
+print(10 + 10, 20-10, 5 * 5, 10 // 2, 5 ** 5)
+print(10 + 10 + 20-10 + 5 * 5 + 10 // 2 + 5 ** 5)
