67e61a4cd8e4c17ea7d8e6d7d88113c4ea4af7fc
luka081009 <lukarijhamadze@gmail.com>
2024-06-25 19:32:23 +0400

    commit

---
+++ level 7/classwork/level7.py
@@ -0,0 +1,24 @@
+# name = input('Enter your name: ')
+# last_name = input('Enter you last_name: ')
+# country = input('Which country do you live: ')
+# city = input('Enter city: ')
+# print('hallo my name is ' + name, last_name + 'I live in ' + country)
+
+# age = input('Enter your age: ')
+# print(age)
+
+# name = 'dato'
+# age = 10
+
+# type()
+# str()
+# int()
+
+# print(type(name))
+# print(type(age))
+
+# x = int(input('Enter x: '))
+# y = int(input('Enter y: '))
+
+# print(x + y)
+
---
+++ level 7/homework/level 7.py
@@ -0,0 +1,55 @@
+
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+c = int(input('Enter c: '))
+d = int(input('Enter d: '))
+
+print(a + c + d -b)
+print(c - b // a)
+print(a * c - b)
+print(c ** b / a)
+print(b / d + a ** c)
+print(b - c - d * a)
+
+
+
+
+
+b = int(input('Enter b: '))
+k = int(input('Enter k: '))
+g = int(input('Enter g: '))
+n = int(input('Enter n: '))
+d = int(input('Enter d: '))
+r = int(input('Enter r: '))
+m = int(input('Enter m: '))
+
+print(b + 20)
+print(k + 20)
+print(g + 20)
+print(n + 20)
+print(d + 20)
+print(r + 20)
+print(m + 20)
+
+
+
+
+name = input('Enter name: ')
+last_name = input('Enter last_name: ')
+age = input('Enter age: ')
+height = input('Enter height: ')
+country = input('Enter country: ')
+city = input('Enter city: ')
+favorite_color = input('Enter favorite_color: ')
+favorite_car = input('Enter favorite_car: ')
+favorite_meal = input('Enter favorite_meal: ')
+favorite_sport = input('Enter favorite_sport: ')
+
+print('My name is ' + name, last_name + '. ' + 'I am ' + age + ' years old, my height is ' + height + 'cm' + '.' +  ' I live in ' + country + ',' + city + '.' + ' My favorite color is ' + favorite_color + ' and ' + 'favorite car ' + favorite_car + '.' + 'I love playing ' + favorite_sport + ' and I love ' + favorite_meal + '.')
+
+
+
+
+you_win = input('enter iour adress to get your gift: ')
+
+print('You win smartphone, enter your adress :' + "adress " + you_win) 
