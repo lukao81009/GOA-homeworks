a8cf55299376db5a1877416b6b149c1a2dce108e
luka081009 <lukarijhamadze@gmail.com>
2024-07-11 13:06:16 +0400

    commit

---
+++ level 9/homework/level 9.py
@@ -0,0 +1,87 @@
+# 1) გააკეთეთ 30 მაგალითი შედარებით ოპერატორებზე. 
+
+# მეტია
+
+print(130 > 125)
+print(432 > 325)
+print(2234 > 1445)
+print(3324 > 664)
+print(33412 > 5332)
+print(88534  > 77593)
+
+# ნაკლებია
+
+print(143 < 543)
+print(453 < 743)
+print(2345 < 5432)
+print(3248 < 4535)
+print(43525 < 543542)
+print(35 < 123)
+ 
+ # მეტი ან ტოლი
+
+print(3444 >= 3444)
+print(12334 >= 1234)
+print(814 >= 84)
+print(452 >= 255)
+print(223 >=223)
+print(543 >= 543)
+
+# ნაკლები ან ტოლი
+
+print(211 <= 1333)
+print(314 <= 314)
+print(234 <= 543)
+print(432 <= 5432)
+print(123 <= 123)
+print(875 <= 875)
+
+# უდრის
+
+print(34744 == 34744)
+print(12334 == 12334)
+print(814 == 814)
+print(253 == 253)
+print(3654 == 3654)
+print(99870 == 99870)
+
+# არ უდრის
+
+print(43252 != 5723)
+print(3452 != 325)
+print(23525 != 235554)
+print(463646 != 4325456)
+print(5675 != 657656)
+print(455656 != 655)
+
+
+
+
+# 2) დაწერეთ პროგრამა, რომელიც იღებს ორი ადამიანის ასაკს და განსაზღვრავს ვინ არის უფროსი.
+
+your_age = int(input("enter your age "))
+dad_age = int(input("enter your dad age "))
+
+print(your_age < dad_age)
+
+
+# 3)
+
+sevings = int(input('enter your sevings'))
+
+five_percent = sevings / 20
+
+final_sevings = sevings + five_percent
+
+print(final_sevings)
+
+
+
+
+# 4)
+
+# the progran takes the score as an imput
+score = int(input())
+
+# add the comparison operation inside the parentheses
+print(score > 100)
