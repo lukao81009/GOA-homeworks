c4cae27e05cac561f0811e7b7a04f48e8255fa8d
luka081009 <lukarijhamadze@gmail.com>
2024-07-24 16:26:05 +0400

    commit

---
+++ level 11/classwork/level11.py
@@ -0,0 +1,25 @@
+# Logical operator:
+# 1) 'and' operator
+# 2) 'or' operator
+
+# 1) პირობა
+# 2) ოპერატორი
+# 3) პირობა
+
+
+# and operator
+
+#      (1) (2) (3)
+print(True and True) # True
+print(True and False) # False
+print(False and True) # False
+print(False and False) # False
+
+
+# or operator
+
+#     (1) (2) (3)
+print(True or True) # True
+print(True or False) # True
+print(False or True) # True
+print(False or False) # False
---
+++ level 11/homework/level 11.py
@@ -0,0 +1,40 @@
+# 1)მომხმარებელს შემოატანინეთ სახელი თუ მისი სახელი თუ უდრის თქვენ სახელს მაშინ გამოგვიტანოს True ხოლო სხვა შემთხვევაში False
+
+name = input('enter your name: ')
+
+if name == 'Luka':
+    print(True)
+else :
+    print(False)
+
+# 2) მომხმარებელს შემატანინეთ რიცხვი თუ რიცხვი იყო 0 მეტი და 10 ნაკლები მაშინ დაგვიპრინტოს True ხოლო სხვა შემთხვევაში False
+
+a = int(input('enter corect number: '))
+
+if a > 0 and a < 10 :
+    print(True)
+else :
+    print(False)
+
+# 3) გაკეთეთ 20 მაგალითი შედარებით ოპერატორებზე
+
+print(20 > 12)
+print(14 < 32)
+print(14 == 14)
+print(123 != 356)
+print(240 <= 423)
+print(244 >= 99)
+print(3123 > 213)
+print(33 < 1323 )
+print(1234 ==1234 )
+print(2144 !=1234 )
+print(2143 <= 3124)
+print(144 >= 144)
+print(412 > 411)
+print(123 < 455)
+print(143 == 143)
+print(24134 != 23243)
+print(325 <= 325)
+print(24442 >= 2341)
+print(325 > 244)
+print(1234 < 2421)
