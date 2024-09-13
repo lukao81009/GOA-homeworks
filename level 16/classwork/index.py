b0e98781041017bfbb2c676cf0ea4b1270652487
luka081009 <lukarijhamadze@gmail.com>
2024-09-13 20:39:47 +0400

    commit

---
+++ level 16/classwork/index.py
@@ -0,0 +1,32 @@
+
+# for i in range(30 + 1):
+#     print(i)
+
+for e in range(1, 10 + 1, 2):
+    print(e)
+
+# for w in range(0, 10 + 1):
+#     print('levani')
+
+# for q in range(0, 10 + 1):
+#     print(str(q) +'. levani')
+
+# for e in range(1, 10 + 1, 2):
+#     name = input('enter your name')
+
+
+# num = int(input('enter number :'))
+
+# for i in range(num + 1):
+#     print(i)
+
+# while True:
+#     print(200)
+
+# num = 0
+# for i in range(10):    
+#     num = num + 1
+#     print(num)
+
+for i in range(2, 10 + 1, 2):
+    print(i)
---
+++ level 16/homework/level 16.py
@@ -0,0 +1,41 @@
+# დაწერეთ პროგრამა რომელიც გამოიტანს მხოლოდ ლუწ რიხვებს.
+
+for i in range(2, 10 + 1, 2):
+    print(i)
+
+# დაწერეთ პროგრამა რომელიც გამოიტანს მხოლოდ კენტ რიცხვებს
+
+for e in range(1, 10 + 1, 2):
+    print(e)
+
+# გაკეთეთ for ლუპზე 10 მაგალითი, და ბევრი იმუშავეთ.
+
+for i in range(1, 25):
+    print(i)
+
+for i in range(0, 11):
+    print(i)
+
+for i in range(15, 50 + 1):
+    print(i)
+
+for i in range(23, 150 + 1, 3):
+    print(i)
+
+for i in range(2, 993 + 1, 10):
+    print(i)
+
+for i in range(83, 838383 + 1, 183):
+    print(i)
+
+for i in range(23, 1234 + 2, 2):
+    print(i)
+
+for i in range(768, 5797567 + 2, 222):
+    print(i)
+
+for i in range(2343, 24134 + 1, 3):
+    print(i)
+
+for i in range(323 + 987 + 1, 4):
+    print(i)
