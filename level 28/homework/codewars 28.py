98b956443816be26569d01255e52a84f6b15bb02
luka081009 <lukarijhamadze@gmail.com>
2024-11-26 20:26:10 +0400

    'commit

---
+++ level 28/homework/codewars 28.py
@@ -0,0 +1,20 @@
+# 1) =================================================================
+
+def boolean_to_string(b):
+    #your code here
+    print(str(b))
+
+# 2) =================================================================
+
+def greet(name):
+    #Good Luck (like you need it)
+    print("Hello, " + name + " how are you doing today?")
+
+# 3) =================================================================
+
+def paperwork(n, m):
+    # Happy Coding! ^_^
+    if n < 0 or m < 0:
+        print(0)
+    else:
+        print(n * m)
